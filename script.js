9d8b3654b74a9ade7c532a1b35fd7790f5cdb8d9
Taras Anpilov <t.anpilov@gmail.com>
2019-08-28 15:26:11 +0300

    add focusing on element

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-const input = document.getElementById('upload_container')
+const field = document.getElementById('upload_container');
+const input = document.getElementById('file_upload');
+
+input.addEventListener('focus', setFocus);
+input.addEventListener('blur', outFocus);
+
+function setFocus() {
+    field.classList.add('in_focus');
+}
+
+function outFocus() {
+    field.classList.remove('in_focus');
+}
