5283e35b1feb70a4c843766b889ec3712afcdcb6
Taras Anpilov <t.anpilov@gmail.com>
2019-08-28 16:08:56 +0300

    add drag-n-drop

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const field = document.getElementById('upload_container');
 const input = document.getElementById('file_upload');
+const extensions = ['xls', 'sxls'];
+const file_size = 10000;
 
 input.addEventListener('focus', setFocus);
 input.addEventListener('blur', outFocus);
@@ -10,4 +12,27 @@ function setFocus() {
 
 function outFocus() {
     field.classList.remove('in_focus');
+}
+
+field.addEventListener('drag', abortDefault)
+field.addEventListener('dragstart', abortDefault)
+field.addEventListener('dragend', abortDefault)
+field.addEventListener('dragover', addDragClass)
+field.addEventListener('dragenter', addDragClass)
+field.addEventListener('dragleave', removeDragClass)
+field.addEventListener('drop', abortDefault)
+
+
+function abortDefault(event) {
+    event.preventDefault()
+}
+
+function addDragClass(event) {
+    abortDefault(event);
+    event.target.classList.add('drag_over')
+}
+
+function removeDragClass(event) {
+    abortDefault(event);
+    event.target.classList.remove('drag_over')
 }
